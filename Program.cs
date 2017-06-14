477fcb1238b9c76f23383a7eff5166b59c49b795
Jerome Cheng <jerome@ayulin.net>
2017-06-14 23:23:25 +0800

    Initial commit.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace ColemakConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputPath = args[0];
+
+            string[] inputLines = File.ReadAllLines(inputPath);
+
+            var converted = inputLines.Select(line => QWERTYDecrypter.QWERTYStringToColemak(line));
+
+            var outputPath = args[1];
+
+            File.WriteAllLines(outputPath, converted);
+        }
+    }
+}
